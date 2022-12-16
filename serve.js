5fa7058a81133f20cf873214e507a85c06d5d4fc
JiaCheng-code <675232630@qq.com>
2022-12-16 23:56:47 +0800

    feat:用户下线

--- serve.js
+++ serve.js
@@ -24,7 +24,7 @@ const io = socket(server, {
     }
 })
 
-const peers = [] // 初始化对等连接数组
+let peers = [] // 初始化对等连接数组
 const broadcastEventType = {
     ACTIVE_USERS: 'ACTIVE_USERS',
     GROUP_CALL_ROOMS: 'GROUP_CALL_ROOMS'
@@ -35,16 +35,29 @@ io.on('connection', (socket) => {
     console.log('新用户加入房间')
     // 注册新用户
     socket.on('register-mew-user', (data) => {
+
         peers.push({
             username: data.username,
             socketId: data.socketId
         })
         console.log('注册新用户', peers)
+        // 向所有连接到客户端的用户广播活跃用户
+        io.sockets.emit('broadcast', {
+            event: broadcastEventType.ACTIVE_USERS,
+            activeUsers:peers
+        })
+    })
+    // 断开连接移除用户
+    socket.on('disconnect',()=>{
+        console.log('用户下线')
+        peers = peers.filter((peer)=>{
+            return peer.socketId !== socket.id
+        })
+        // 向所有连接到客户端的用户广播活跃用户
+        io.sockets.emit('broadcast', {
+            event: broadcastEventType.ACTIVE_USERS,
+            activeUsers:peers
+        })
     })
 
-    // 向所有连接到客户端的用户广播活跃用户
-    socket.emit('broadcast', {
-        event: broadcastEventType.ACTIVE_USERS,
-        activeUsers:peers
-    })
 })
